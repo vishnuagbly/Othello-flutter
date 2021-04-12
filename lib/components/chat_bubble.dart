0e930f389e28df2e7b75e3989d619243add7cc8a
raunak_k02 <raunakk728@gmail.com>
2021-04-13 01:40:07 +0530

    adds timestamp to chat bubble

--- lib/components/chat_bubble.dart
+++ lib/components/chat_bubble.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:bubble/bubble.dart';
+import 'package:intl/intl.dart';
 import 'package:othello/objects/chat_message.dart';
 
 class ChatBubble extends StatelessWidget {
@@ -16,11 +17,27 @@ class ChatBubble extends StatelessWidget {
       child: Bubble(
         showNip: showNip,
         nip: isSender ? BubbleNip.rightTop : BubbleNip.leftTop,
-        child: Text(
-          message.msg,
-          style: TextStyle(
-            color: Colors.black,
-          ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              message.msg,
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            Text(
+              DateFormat.jm().format(message.timestamp),
+              textAlign: TextAlign.right,
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.grey[800],
+              ),
+            ),
+          ],
         ),
         alignment: isSender ? Alignment.topRight : Alignment.topLeft,
         color: isSender ? Colors.grey : Colors.white,
